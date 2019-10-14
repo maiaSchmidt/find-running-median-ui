7c45f824b4e3f117fd0cc4c33748552aded600dc
fabioms <fabiomaiaschmidt@iheartmedia.com>
2019-10-13 22:20:50 -0300

    Tests fixed.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MatFormFieldModule } from '@angular/material';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatFormFieldModule,
+        HttpClientTestingModule
       ],
       declarations: [
         AppComponent
@@ -20,16 +24,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FindingRunningMedian'`, () => {
+  it(`should have as title 'Calculate Incremental Median'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('FindingRunningMedian');
+    expect(app.title).toEqual('Calculate Incremental Median');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to FindingRunningMedian!');
+    expect(compiled.querySelector('h1').textContent).toContain('Calculate Incremental Median');
   });
 });
--- src/app/services/median.service.spec.ts
+++ src/app/services/median.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-
 import { MedianService } from './median.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('MedianServiceService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule
+    ],
+  }));
 
   it('should be created', () => {
     const service: MedianService = TestBed.get(MedianService);
